2cfea0569391a986bbdc6ee76a9f79620be3248c
Anish Shrestha <me.anish.shrestha@gmail.com>
2022-06-14 09:38:46 +0545

    oop add

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-const carouselContainerWidth = 600;
-const carouselContainerHeight = 400;
-
 function toPixel(number) {
   return `${number}px`;
 }
 
 const body = document.body;
-const carouselContainer = document.getElementById("carousel-container");
-const carouselImageWrapper = document.getElementById("carousel-image-wrapper");
 
-// carouselContainer styling
-carouselContainer.style.width = toPixel(carouselContainerWidth);
-carouselContainer.style.height = toPixel(carouselContainerHeight);
-carouselContainer.style.margin = "auto";
-carouselContainer.style.position = "relative";
-carouselContainer.style.overflow = "hidden";
-
-// carouselImageWrapper styling
-carouselImageWrapper.style.position = "absolute";
-carouselImageWrapper.style.top = "0px";
-
-carouselImageWrapper.style.height = toPixel(carouselContainerHeight);
-carouselImageWrapper.style.width = "100%";
-
-// images styling
-const childrenImagesNodes = carouselImageWrapper.children;
-const childrenImages = [...childrenImagesNodes];
-childrenImages.forEach((image, index) => {
-  image.id = index;
-  image.style.position = "absolute";
-  image.style.top = "0px";
-  image.style.left = toPixel(index * carouselContainerWidth);
-});
-
-function moveImage(previousIndex, currentIndex, speed = 10) {
+//moving image function global
+function moveImage(
+  previousIndex,
+  currentIndex,
+  speed = 10,
+  carouselContainerWidth,
+  carouselContainerHeight,
+  carouselImageWrapper
+) {
   if (previousIndex < currentIndex) {
     let previousPosition = -previousIndex * carouselContainerWidth;
     let currentPosition = previousPosition;
@@ -64,8 +43,8 @@ function moveImage(previousIndex, currentIndex, speed = 10) {
     play();
   }
 }
-function changeActiveDot(currentIndex) {
-  let dots = [...document.getElementById("dot-container").children];
+function changeActiveDot(currentIndex, dotsContainer) {
+  let dots = [...dotsContainer.children];
   dots.forEach((dot) => {
     if (currentIndex === parseInt(dot.id)) {
       dot.classList.add("active");
@@ -83,7 +62,38 @@ class Carousel {
   constructor(speed = 10) {
     this.speed = speed;
     this.currentIndex = 0;
+    this.carouselContainerWidth = 600;
+    this.carouselContainerHeight = 400;
+    this.carouselContainer = document.getElementById("carousel-container");
+    this.carouselImageWrapper = document.getElementById(
+      "carousel-image-wrapper"
+    );
+    // carouselContainer styling
+    this.carouselContainer.style.width = toPixel(this.carouselContainerWidth);
+    this.carouselContainer.style.height = toPixel(this.carouselContainerHeight);
+    this.carouselContainer.style.margin = "auto";
+    this.carouselContainer.style.position = "relative";
+    this.carouselContainer.style.overflow = "hidden";
+    // carouselImageWrapper styling
+    this.carouselImageWrapper.style.position = "absolute";
+    this.carouselImageWrapper.style.top = "0px";
+
+    this.carouselImageWrapper.style.height = toPixel(
+      this.carouselContainerHeight
+    );
+    this.carouselImageWrapper.style.width = "100%";
+
+    // images styling
+    this.childrenImagesNodes = this.carouselImageWrapper.children;
+    this.childrenImages = [...this.childrenImagesNodes];
+    this.childrenImages.forEach((image, index) => {
+      image.id = index;
+      image.style.position = "absolute";
+      image.style.top = "0px";
+      image.style.left = toPixel(index * this.carouselContainerWidth);
+    });
   }
+  styleCarousel() {}
   addButtons() {
     this.leftButton = document.createElement("img");
     this.leftButton.setAttribute("src", "./assests/arrow-left-solid.svg");
@@ -125,8 +135,8 @@ class Carousel {
       this.moveRight();
     });
 
-    carouselContainer.appendChild(this.leftButton);
-    carouselContainer.appendChild(this.rightButton);
+    this.carouselContainer.appendChild(this.leftButton);
+    this.carouselContainer.appendChild(this.rightButton);
   }
 
   addDots() {
@@ -135,67 +145,103 @@ class Carousel {
     this.dotContainer.style.position = "absolute";
     this.dotContainer.style.bottom = "0px";
     this.dotContainer.style.left = `calc(50% - ${
-      (50 * childrenImages.length) / 2
+      (50 * this.childrenImages.length) / 2
     }px`;
 
-    for (let i = 0; i < childrenImages.length; i++) {
-      this.dot = document.createElement("img");
-      this.dot.id = i;
+    for (let i = 0; i < this.childrenImages.length; i++) {
+      let dot = document.createElement("img");
+      dot.id = i;
       if (i === 0) {
-        this.dot.classList.add("active");
-        this.dot.style.filter =
+        dot.classList.add("active");
+        dot.style.filter =
           "invert(42%) sepia(93%) saturate(1352%) hue-rotate(87deg) brightness(119%) contrast(119%)";
       }
-      this.dot.setAttribute("src", "./assests/dot-circle-solid.svg");
-      this.dot.style.width = "50px";
-      this.dot.style.height = "50px";
+      dot.setAttribute("src", "./assests/dot-circle-solid.svg");
+      dot.style.width = "50px";
+      dot.style.height = "50px";
 
-      this.dot.addEventListener("mouseenter", () => {
-        this.dot.style.filter =
+      dot.addEventListener("mouseenter", () => {
+        dot.style.filter =
           "invert(42%) sepia(93%) saturate(1352%) hue-rotate(87deg) brightness(119%) contrast(119%)";
       });
-      this.dot.addEventListener("mouseleave", () => {
-        if (!this.dot.classList.contains("active")) {
-          this.dot.style.filter =
+      dot.addEventListener("mouseleave", () => {
+        if (!dot.classList.contains("active")) {
+          dot.style.filter =
             "invert(0) sepia(0) saturate(0) hue-rotate(0) brightness(0) contrast(119%)";
         }
       });
-      this.dot.addEventListener("click", (event) => {
-        carousel.goto(event.target.id);
+      dot.addEventListener("click", (event) => {
+        this.goto(event.target.id, this.dotContainer);
       });
 
-      this.dotContainer.appendChild(this.dot);
+      this.dotContainer.appendChild(dot);
     }
 
-    carouselContainer.appendChild(this.dotContainer);
+    this.carouselContainer.appendChild(this.dotContainer);
   }
   moveRight() {
-    if (this.currentIndex === childrenImages.length - 1) {
-      moveImage(this.currentIndex, 0, 30);
+    if (this.currentIndex === this.childrenImages.length - 1) {
+      moveImage(
+        this.currentIndex,
+        0,
+        30,
+        this.carouselContainerWidth,
+        this.carouselContainerHeight,
+        this.carouselImageWrapper
+      );
       this.currentIndex = 0;
     } else {
-      moveImage(this.currentIndex, this.currentIndex + 1, this.speed);
+      moveImage(
+        this.currentIndex,
+        this.currentIndex + 1,
+        this.speed,
+        this.carouselContainerWidth,
+        this.carouselContainerHeight,
+        this.carouselImageWrapper
+      );
       this.currentIndex += 1;
     }
-    changeActiveDot(this.currentIndex);
+    changeActiveDot(this.currentIndex, this.dotContainer);
   }
   moveLeft() {
     if (this.currentIndex === 0) {
-      moveImage(this.currentIndex, childrenImages.length - 1, 30);
-      this.currentIndex = childrenImages.length - 1;
+      moveImage(
+        this.currentIndex,
+        this.childrenImages.length - 1,
+        30,
+        this.carouselContainerWidth,
+        this.carouselContainerHeight,
+        this.carouselImageWrapper
+      );
+      this.currentIndex = this.childrenImages.length - 1;
     } else {
-      moveImage(this.currentIndex, this.currentIndex - 1, this.speed);
+      moveImage(
+        this.currentIndex,
+        this.currentIndex - 1,
+        this.speed,
+        this.carouselContainerWidth,
+        this.carouselContainerHeight,
+        this.carouselImageWrapper
+      );
       this.currentIndex -= 1;
     }
-    changeActiveDot(this.currentIndex);
+    changeActiveDot(this.currentIndex, this.dotContainer);
   }
-  goto(currentIndex) {
-    moveImage(this.currentIndex, parseInt(currentIndex), 30);
+  goto(currentIndex, dotsContainer) {
+    moveImage(
+      this.currentIndex,
+      parseInt(currentIndex),
+      30,
+      this.carouselContainerWidth,
+      this.carouselContainerHeight,
+      this.carouselImageWrapper
+    );
     this.currentIndex = parseInt(currentIndex);
-    changeActiveDot(this.currentIndex);
+    changeActiveDot(this.currentIndex, dotsContainer);
   }
 }
 
 let carousel = new Carousel();
 carousel.addButtons();
+carousel.styleCarousel();
 carousel.addDots();
